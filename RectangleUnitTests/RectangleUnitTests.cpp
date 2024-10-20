57498971de9672065e411f749f9a9ca5160d90bf
Dylan Litt <dylanlitt2006@gmail.com>
2024-10-20 16:24:54 -0400

    Added 6 unit tests.

--- RectangleUnitTests/RectangleUnitTests.cpp
+++ RectangleUnitTests/RectangleUnitTests.cpp
@@ -32,5 +32,65 @@ namespace RectangeUnitTests
             Assert::AreEqual(15, area);
         }
 
+        TEST_METHOD(TestSetLength_ValidInput)
+        {
+            int length = 0;
+            int input = 50;
+
+            setLength(input, &length);
+
+            Assert::AreEqual(50, length);
+        }
+
+        TEST_METHOD(TestSetLength_MinEdgeCase)
+        {
+            int length = 0;
+            int input = 1;
+
+            setLength(input, &length);
+
+            Assert::AreEqual(1, length);
+        }
+
+        TEST_METHOD(TestSetLength_BelowMin)
+        {
+            int length = 10;
+            int input = 0;
+
+
+            setLength(input, &length);
+
+            Assert::AreEqual(10, length);
+        }
+
+        TEST_METHOD(TestSetWidth_ValidInput)
+        {
+            int width = 0;
+            int input = 70;
+
+            setWidth(input, &width);
+
+            Assert::AreEqual(70, width);
+        }
+
+        TEST_METHOD(TestSetWidth_MaxEdgeCase)
+        {
+            int width = 0;
+            int input = 99;
+
+            setWidth(input, &width);
+
+            Assert::AreEqual(99, width);
+        }
+
+        TEST_METHOD(TestSetWidth_AboveMax)
+        {
+            int width = 20;
+            int input = 100;
+
+            setWidth(input, &width);
+
+            Assert::AreEqual(20, width);
+        }
     };
 }
